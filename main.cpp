221c3d52bb5b9eef8413dddcaf2b65e4ebe1a0fc
dilbud <buddikadilan3@gmail.com>
2023-07-07 17:05:41 +0000

    test without server uni stream

--- main.cpp
+++ main.cpp
@@ -14,79 +14,161 @@
 #include "owt/quic/web_transport_definitions.h"
 #include "owt/quic/web_transport_stream_interface.h"
 
-class StreamEchoVisitor : public owt::quic::WebTransportStreamInterface::Visitor {
- public:
-  explicit StreamEchoVisitor(owt::quic::WebTransportStreamInterface* stream) : stream_(stream) {
+class StreamEchoVisitor
+    : public owt::quic::WebTransportStreamInterface::Visitor {
+   public:
+    explicit StreamEchoVisitor(owt::quic::WebTransportSessionInterface *session,
+                               owt::quic::WebTransportStreamInterface *stream)
+        : session_(session), stream_(stream) {}
+    void OnCanWrite() override { std::printf("stream can write \n"); }
+    void OnCanRead() override {
+        std::printf("stream can read \n");
+        std::printf("stream ID %d \n", stream_->Id());
+        std::printf("stream WRITABLE %d \n", stream_->CanWrite());
+        if (stream_->CanWrite()) {
+            std::string sstr= std::string("hello world from server");
+            std::vector<uint8_t> vec(sstr.begin(), sstr.end());
+            size_t rs = stream_->Write(vec.data(), sstr.size());
+            std::printf("stream write size %ld \n", rs);
+        }
+        std::printf("stream readable BYTE %ld \n", stream_->ReadableBytes());
+        std::printf("stream Buffer B_BYTE %ld \n", stream_->BufferedDataBytes());
+        size_t read_size = stream_->ReadableBytes();
+        if (read_size > 0) {
+            std::vector<uint8_t> data(read_size);
+            stream_->Read(data.data(), read_size);
+            std::string str;
+            str.assign(data.data(), data.data() + read_size);
+            std::printf("received msg: %s \n", str.c_str());
+            std::printf("received msg length: %lu\n", str.size());
+        }
+    }
+    void OnFinRead() override { std::printf("FIN READ \n"); }
 
-  }
-  void OnCanWrite() override {}
-  void OnCanRead() override {
-      auto read_size = stream_->ReadableBytes();
-      if (read_size == 0)
-      {
-          return;
-      }
-      std::vector<uint8_t> data(read_size);
-      stream_->Read(data.data(), read_size);
-      stream_->Write(data.data(), read_size);
-  }
-  void OnFinRead() override {}
-
- private:
-  owt::quic::WebTransportStreamInterface* stream_;
+   private:
+    owt::quic::WebTransportSessionInterface *session_;
+    owt::quic::WebTransportStreamInterface *stream_;
 };
-
 
 class SessionEchoVisitor : public owt::quic::WebTransportSessionInterface::Visitor {
  public:
-  explicit SessionEchoVisitor(owt::quic::WebTransportSessionInterface* si) : si_(si) {
+  explicit SessionEchoVisitor(owt::quic::WebTransportSessionInterface *session)
+      : session_(session) {}
+  ~SessionEchoVisitor() {}
 
+  void OnCanCreateNewOutgoingStream(bool ) override {
+    std::printf("can create outgoing stream \n");
   }
-  ~SessionEchoVisitor() {
-
+  void OnConnectionClosed() override {
+    std::printf("Con Closed \n");
   }
-
-  void OnCanCreateNewOutgoingStream(bool) override {}
-  void OnConnectionClosed() override {}
   void OnIncomingStream(owt::quic::WebTransportStreamInterface *stream) override {
-      std::unique_ptr<StreamEchoVisitor> visitor =
-          std::make_unique<StreamEchoVisitor>(stream);
-      stream->SetVisitor(visitor.get());
-      stream_visitors_.push_back(std::move(visitor));
+      std::printf("\nstart...............OnIncomingStream\n\n");
+      std::printf("stream creating\n");
+      std::printf("stream ID %d \n", stream->Id());
+      StreamEchoVisitor* visitor = new StreamEchoVisitor(session_, stream);
+      stream->SetVisitor(visitor);
+      stream_visitors_.emplace_back(visitor);
+      std::printf("\nend...............OnIncomingStream\n\n");
   }
-  void OnDatagramReceived(const uint8_t* data, size_t length) override {
-    si_->SendOrQueueDatagram((unsigned char*)data, length);
+
+  void OnDatagramReceived(const uint8_t *data, size_t length) override {
+      std::printf("\nstart...............OnDatagramReceived\n\n");
+      std::printf("session ID %s \n", session_->ConnectionId());
+      std::string str;
+      str.assign(data, data + length);
+      std::printf("received msg: %s \n",  str.c_str());
+      std::printf("received msg length: %lu\n", length);
+      owt::quic::MessageStatus ms =
+          session_->SendOrQueueDatagram((unsigned char *)data, length);
+      using namespace owt::quic;
+      std::printf("msg send \n");
+      switch (ms) {
+            case MessageStatus::kSuccess:
+            std::printf("Success\n");
+            break;
+            case MessageStatus::kEncryptionNotEstablished:
+            std::printf("Failed to send message because encryption is not established yet\n");
+            break;
+            case MessageStatus::kUnsupported:
+            std::printf("Failed to send message because MESSAGE frame is not supported by the connection\n");
+            break;
+            case MessageStatus::kBlocked:
+            std::printf("Failed to send message because connection is congestion control blocked or underlying socket is write blocked\n");
+            break;
+            case MessageStatus::kTooLarge:
+            std::printf("Failed to send message because the message is too large to fit into a single packet\n");
+            break;
+            case MessageStatus::kInternalError:
+            std::printf("Failed to send message because connection reaches an invalid state\n");
+            break;
+            case MessageStatus::kUnavailable:
+            std::printf("Message status is not available. When C++17 std::optional is enabled, this value will be removed\n");
+            break;
+            default:
+            std::printf("Unknown\n");
+      }
+      const char* ch = "dddddddddddd";
+      owt::quic::WebTransportStreamInterface * bistream = session_->CreateBidirectionalStream();
+      StreamEchoVisitor* visitor = new StreamEchoVisitor(session_, bistream);
+      bistream->SetVisitor(visitor);
+      std::printf("\nend...............OnDatagramReceived\n\n");
   }
 
  private:
-  std::vector<std::unique_ptr<StreamEchoVisitor>> stream_visitors_;
-  owt::quic::WebTransportSessionInterface* si_; 
+  std::vector<StreamEchoVisitor *> stream_visitors_;
+  owt::quic::WebTransportSessionInterface* session_; 
 };
 
 class ServerEchoVisitor
     : public owt::quic::WebTransportServerInterface::Visitor {
  private:
-  std::vector<owt::quic::WebTransportSessionInterface *> sessions_;
+  std::vector<SessionEchoVisitor *> sessionVisitors_;
 
  public:
   explicit ServerEchoVisitor() {
-        std::printf("sssssssssssssssss");
   }
   ~ServerEchoVisitor() {
-        std::printf("kkkkkkkkkkkkkkkkk");
   }
 
   void OnEnded() override {
-        std::printf("yyyyyyyyyyyyyyyyyyyyyyyyyy");
+        std::printf("Session end\n");
   }
   void OnSession(owt::quic::WebTransportSessionInterface *session) override {
+      std::printf("\nstart...............OnSession\n\n");
+      std::printf("session creating\n");
+      std::printf("session ID %s \n", session->ConnectionId());
       SessionEchoVisitor *visitor = new SessionEchoVisitor(session);
       session->SetVisitor(visitor);
-      sessions_.emplace_back(session);
+      sessionVisitors_.emplace_back(visitor);
+
+    //   while(true) {
+    //   uint64_t i = session->GetStats().estimated_bandwidth;
+    //   std::printf("xxxxxxxxxxxxxxx %lu \n", i);
+    //   }
+    //   if (session->IsSessionReady()) {
+    //     //   std::printf("zzzzzzzzzzzzzzzzzzzzzzzz0\n");
+    //     //   std::string s = std::string("sgfsdfsdf");
+    //     //   owt::quic::MessageStatus ms = session->SendOrQueueDatagram(
+    //     //       (unsigned char *)s.c_str(), s.size());
+    //     //       printf("My enum Value sssssssssssssssss\n");
+    //     //   printf("My enum Value : %d\n", (int)ms);
+
+    //     while (true)
+    //     {
+    //       std::string s = std::string("sgfsdfsdf");
+    //       owt::quic::MessageStatus ms = session->SendOrQueueDatagram(
+    //           (unsigned char *)s.c_str(), s.size());
+    //           printf("My enum Value sssssssssssssssss\n");
+    //       printf("My enum Value : %d\n", (int)ms);
+    //     }
+        
+    //   }
+    std::printf("\nend...............OnSession\n\n");
   }
 
-  std::vector<owt::quic::WebTransportSessionInterface *> Sessions() const {
-      return sessions_;
+  std::vector<SessionEchoVisitor *> SessionVisitors() const {
+      return sessionVisitors_;
   }
 };
 
@@ -115,11 +197,22 @@ int main(int, char **)
     } else {
         std::printf("server terminated\n");
     }
-    while (true)
-    {
-       std::printf("session size: %lu", sv->Sessions().size());
+    bool isc = false;
+    while (true) {
+        // std::printf("session size: %lu\n", sv->SessionVisitors().size());
+        // if (sv->Sessions().size() > 0) {
+        //   if (!isc) {
+        //       std::string s = std::string("sgfsdfsdf");
+        //       owt::quic::WebTransportStreamInterface *bis =
+        //           sv->Sessions().front()->CreateBidirectionalStream();
+        //       if (bis->CanWrite()) {
+        //           bis->Write((unsigned char *)s.c_str(), s.size());
+        //         //   std::printf("write %ld\n", rs);
+        //       }
+        //       isc = true;
+        //   }
+        // }
     }
-    
 
     return 0;
 }
--- webClient/client.js
+++ webClient/client.js
@@ -1,48 +1,75 @@
 // Adds an entry to the event log on the page, optionally applying a specified
 // CSS class.
 
-let currentTransport, streamNumber, currentTransportDatagramWriter;
+let currentTransport;
+let unistreamNumber;
+let bistreamNumber;
+let openbistreamNumber;
+let currentTransportDatagramWriter;
 
 // "Connect" button handler.
 async function connect() {
   const url = document.getElementById('url').value;
   try {
     var transport = new WebTransport(url);
-    addToEventLog('Initiating connection...');
+    transport.closed
+    .then(() => {
+      addToEventLog('Connection closed normally.', 'info', 'connection');
+    })
+    .catch(() => {
+      addToEventLog('Connection closed abruptly.', 'error', 'connection');
+    });
+    addToEventLog('Initiating connection...', 'info', 'connection');
+    try {
+      await transport.ready;
+      addToEventLog('Connection ready.', 'info', 'connection');
+      afterCon(transport);
+    } catch (e) {
+      addToEventLog('Connection failed. ' + e, 'error', 'connection');
+      return;
+    }
   } catch (e) {
-    addToEventLog('Failed to create connection object. ' + e, 'error');
+    addToEventLog('Failed to create connection object. ' + e, 'error', 'connection');
     return;
   }
+}
 
-  try {
-    await transport.ready;
-    addToEventLog('Connection ready.');
-  } catch (e) {
-    addToEventLog('Connection failed. ' + e, 'error');
-    return;
-  }
-
-  transport.closed
-      .then(() => {
-        addToEventLog('Connection closed normally.');
-      })
-      .catch(() => {
-        addToEventLog('Connection closed abruptly.', 'error');
-      });
-
+function afterCon(transport) {
   currentTransport = transport;
-  streamNumber = 1;
-  try {
-    currentTransportDatagramWriter = transport.datagrams.writable.getWriter();
-    addToEventLog('Datagram writer ready.');
-  } catch (e) {
-    addToEventLog('Sending datagrams not supported: ' + e, 'error');
-    return;
-  }
+  unistreamNumber = 1;
+  bistreamNumber = 1;
+  openbistreamNumber = 1;
+  prepareDatagramWriter(transport);
   readDatagrams(transport);
   acceptUnidirectionalStreams(transport);
+  acceptBidirectionalStreams(transport)
+  document.forms.sendingdatagrams.elements.senddatagram.disabled = false;
   document.forms.sending.elements.send.disabled = false;
   document.getElementById('connect').disabled = true;
+}
+
+function prepareDatagramWriter(transport) {
+  try {
+    currentTransportDatagramWriter = transport.datagrams.writable.getWriter();
+    addToEventLog('Datagram writer ready.', 'info', 'datagram-outgoin');
+  } catch (e) {
+    addToEventLog('Sending datagrams not supported: ' + e, 'error', 'datagram-outgoin');
+    return;
+  }
+
+}
+
+async function sendDatagram() {
+  try {
+    let form = document.forms.sendingdatagrams.elements;
+    let rawData = form.datagram.value;
+    let encoder = new TextEncoder('utf-8');
+    let data = encoder.encode(rawData);
+    await currentTransportDatagramWriter.write(data);
+    addToEventLog('Sent datagram: ' + rawData, 'info', 'datagram-outgoin');
+  } catch (e) {
+    addToEventLog('Error while sending datagram: ' + e, 'error', 'datagram-outgoin');
+  }
 }
 
 // "Send data" button handler.
@@ -54,34 +81,50 @@ async function sendData() {
   let transport = currentTransport;
   try {
     switch (form.sendtype.value) {
-      case 'datagram':
-        await currentTransportDatagramWriter.write(data);
-        addToEventLog('Sent datagram: ' + rawData);
-        break;
       case 'unidi': {
-        let stream = await transport.createUnidirectionalStream();
-        let writer = stream.getWriter();
+        const stream = await transport.createUnidirectionalStream();
+        const writer = stream.getWriter();
         await writer.write(data);
-        await writer.close();
-        addToEventLog('Sent a unidirectional stream with data: ' + rawData);
+        addToEventLog('Sent a unidirectional stream with data: ' + rawData, 'info', 'open-stream-uni');
+        try {
+          await writer.close();
+          addToEventLog('stream closed normally.', 'info', 'open-stream-uni');
+        } catch (error) {
+          addToEventLog('stream closed error.', 'error', 'open-stream-uni');
+        }
         break;
       }
       case 'bidi': {
         let stream = await transport.createBidirectionalStream();
-        let number = streamNumber++;
-        readFromIncomingStream(stream, number);
-
+        let number = openbistreamNumber++;
+        readFromIncomingOpenStream(stream.readable, number);
         let writer = stream.writable.getWriter();
         await writer.write(data);
         await writer.close();
-        addToEventLog(
-            'Opened bidirectional stream #' + number +
-            ' with data: ' + rawData);
+        addToEventLog('Opened bidirectional stream #' + number +' with data: ' + rawData, 'info', 'open-stream-bi');
         break;
       }
     }
   } catch (e) {
-    addToEventLog('Error while sending data: ' + e, 'error');
+    addToEventLog('Error while sending data: ' + e, 'error', 'open-stream-uni');
+  }
+}
+
+async function readFromIncomingOpenStream(readable, number) {
+  let decoder = new TextDecoderStream('utf-8');
+  let reader = readable.pipeThrough(decoder).getReader();
+  try {
+    while (true) {
+      const { value, done } = await reader.read();
+      if (done) {
+        addToEventLog('Stream #' + number + ' closed', 'info', 'open-stream-bi');
+        break;
+      }
+      let data = value;
+      addToEventLog('Received data on open bi stream #' + number + ': ' + data, 'info', 'open-stream-bi');
+    }
+  } catch (e) {
+    addToEventLog('Error while reading from stream #' + number + ': ' + e + ' : ' + e.message, 'error', 'open-stream-bi');
   }
 }
 
@@ -89,68 +132,131 @@ async function sendData() {
 async function readDatagrams(transport) {
   try {
     var reader = transport.datagrams.readable.getReader();
-    addToEventLog('Datagram reader ready.');
-  } catch (e) {
-    addToEventLog('Receiving datagrams not supported: ' + e, 'error');
-    return;
-  }
-  let decoder = new TextDecoder('utf-8');
-  try {
-    while (true) {
-      const { value, done } = await reader.read();
-      if (done) {
-        addToEventLog('Done reading datagrams!');
-        return;
+    addToEventLog('Datagram reader ready.', 'info', 'datagram-incomming');
+    let decoder = new TextDecoder('utf-8');
+    try {
+      while (true) {
+        const { value, done } = await reader.read();
+        if (done) {
+          addToEventLog('Done reading datagrams!', 'info', 'datagram-incomming');
+          return;
+        }
+        let data = decoder.decode(value);
+        addToEventLog('Datagram received: ' + data, 'info', 'datagram-incomming');
       }
-      let data = decoder.decode(value);
-      addToEventLog('Datagram received: ' + data);
+    } catch (e) {
+      addToEventLog('Error while reading datagrams: ' + e, 'error', 'datagram-incomming');
     }
   } catch (e) {
-    addToEventLog('Error while reading datagrams: ' + e, 'error');
+    addToEventLog('Receiving datagrams not supported: ' + e, 'error', 'datagram-incomming');
+    return;
   }
 }
 
 async function acceptUnidirectionalStreams(transport) {
-  let reader = transport.incomingUnidirectionalStreams.getReader();
   try {
-    while (true) {
-      const { value, done } = await reader.read();
-      if (done) {
-        addToEventLog('Done accepting unidirectional streams!');
-        return;
+    const reader = transport.incomingUnidirectionalStreams.getReader();
+    addToEventLog('uni stream reader ready.', 'info', 'stream-uni');
+    try {
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) {
+          addToEventLog('Done accepting unidirectional streams!', 'info', 'stream-uni');
+          return;
+        }
+        let stream = value;
+        let number = unistreamNumber++;
+        addToEventLog('New incoming unidirectional stream #' + number, 'info', 'stream-uni');
+        readFromIncomingUniStream(stream, number);
       }
-      let stream = value;
-      let number = streamNumber++;
-      addToEventLog('New incoming unidirectional stream #' + number);
-      readFromIncomingStream(stream, number);
+    } catch (e) {
+      addToEventLog('Error while accepting streams: ' + e, 'error', 'stream-uni');
     }
   } catch (e) {
-    addToEventLog('Error while accepting streams: ' + e, 'error');
+    addToEventLog('Error unidirectional stream read: ' + e, 'error', 'stream-uni');
   }
 }
 
-async function readFromIncomingStream(stream, number) {
+async function readFromIncomingUniStream(stream, number) {
   let decoder = new TextDecoderStream('utf-8');
   let reader = stream.pipeThrough(decoder).getReader();
   try {
     while (true) {
-      const { value, done } = await reader.read();
+      const { done, value } = await reader.read();
       if (done) {
-        addToEventLog('Stream #' + number + ' closed');
+        addToEventLog('Stream #' + number + ' closed', 'info', 'stream-uni');
         return;
       }
       let data = value;
-      addToEventLog('Received data on stream #' + number + ': ' + data);
+      addToEventLog('Received data on uni stream #' + number + ': ' + data, 'info', 'stream-uni');
     }
   } catch (e) {
-    addToEventLog(
-        'Error while reading from stream #' + number + ': ' + e, 'error');
-    addToEventLog('    ' + e.message);
+    addToEventLog('Error while reading from stream #' + number + ': ' + e + ' : ' + e.message, 'error', 'stream-uni');
   }
 }
 
-function addToEventLog(text, severity = 'info') {
-  let log = document.getElementById('event-log');
+async function acceptBidirectionalStreams(transport) {
+  try {
+    const reader = transport.incomingBidirectionalStreams.getReader();
+    addToEventLog('bi stream reader ready.', 'info', 'stream-bi');
+    try {
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) {
+          addToEventLog('Done accepting bidirectional streams!', 'info', 'stream-bi');
+          return;
+        }
+        let stream = value;
+        let number = bistreamNumber++;
+        addToEventLog('New incoming biidirectional stream #' + number, 'info', 'stream-bi');
+        readData(value.readable, number);
+        writeData(value.writable, number);
+      }
+    } catch (e) {
+      addToEventLog('Error while accepting streams: ' + e, 'error', 'stream-bi');
+    }
+  } catch (e) {
+    addToEventLog('Error unidirectional stream reader: ' + e, 'error', 'stream-bi');
+  }
+}
+
+async function readData(readable, number) {
+  let decoder = new TextDecoderStream('utf-8');
+  const reader = readable.pipeThrough(decoder).getReader();
+  try {
+    while (true) {
+      const { value, done } = await reader.read();
+      if (done) {
+        addToEventLog('Stream #' + number + ' closed', 'info', 'stream-bi');
+        break;
+      }
+      let data = value;
+      addToEventLog('Received data on bi stream #' + number + ': ' + data, 'info', 'stream-bi');
+    }
+  } catch (e) {
+    addToEventLog('Error while reading from stream #' + number + ': ' + e + ' : ' + e.message, 'error', 'stream-bi');
+  }
+}
+
+async function writeData(writable, number) {
+  try {
+    const writer = writable.getWriter();
+    let encoder = new TextEncoder('utf-8');
+    let rawData1 = 'hello ';
+    let data1 = encoder.encode(rawData1);
+    let rawData2 = 'world';
+    let data2 = encoder.encode(rawData2);
+    writer.write(data1);
+    addToEventLog('write to incomming bidirectional stream #' + number +' with data: ' + rawData1, 'info', 'stream-bi');
+    writer.write(data2);
+    addToEventLog('write to incomming bidirectional stream #' + number +' with data: ' + rawData2, 'info', 'stream-bi');
+  } catch (e) {
+    addToEventLog('Error write to stream #' + number + ': ' + e + ' : ' + e.message, 'error', 'stream-bi');
+  }
+}
+
+function addToEventLog(text, severity = 'info', id = 'event') {
+  let log = document.getElementById(`${id}-log`);
   let mostRecentEntry = log.lastElementChild;
   let entry = document.createElement('li');
   entry.innerText = text;
