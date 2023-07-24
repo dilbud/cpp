ded30b3a60f48b51180e9dd6bf64f5aaa6215015
dilbud <buddikadilan3@gmail.com>
2023-07-24 19:39:30 +0000

    add uv and srt

--- cmake/Findsrt.cmake
+++ cmake/Findsrt.cmake
@@ -10,3 +10,6 @@ target_include_directories( srt
                             INTERFACE ${CMAKE_SOURCE_DIR}/extern/srt
                             INTERFACE ${CMAKE_SOURCE_DIR}/extern/srt/common
                             INTERFACE ${CMAKE_SOURCE_DIR}/extern/srt/srtcore )
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(srt DEFAULT_MSG)
--- cmake/Finduv.cmake
+++ cmake/Finduv.cmake
@@ -1,15 +1,29 @@
-include(FetchContent)
+# include(FetchContent)
 
-FetchContent_Declare(   libuv
-                        GIT_REPOSITORY https://github.com/libuv/libuv.git
-                        GIT_TAG v1.46.0
-                        GIT_SHALLOW 1 )
+# FetchContent_Declare(   libuv
+#                         GIT_REPOSITORY https://github.com/libuv/libuv.git
+#                         GIT_TAG v1.46.0
+#                         GIT_SHALLOW 1 )
 
-FetchContent_GetProperties(libuv)
+# FetchContent_GetProperties(libuv)
 
-if(NOT libuv_POPULATED)
-    FetchContent_Populate(libuv)
-    add_subdirectory(   ${libuv_SOURCE_DIR}
-                        ${libuv_BINARY_DIR}
-                        EXCLUDE_FROM_ALL )
-endif()
+# if(NOT libuv_POPULATED)
+#     FetchContent_Populate(libuv)
+#     add_subdirectory(   ${libuv_SOURCE_DIR}
+#                         ${libuv_BINARY_DIR}
+#                         EXCLUDE_FROM_ALL )
+# endif()
+
+add_library( uv
+             SHARED
+             IMPORTED )
+
+set_target_properties( uv
+                       PROPERTIES IMPORTED_LOCATION
+                       ${CMAKE_SOURCE_DIR}/extern/uv/build/libuv.so )
+
+target_include_directories( uv
+                            INTERFACE ${CMAKE_SOURCE_DIR}/extern/uv/include )
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(uv DEFAULT_MSG)
