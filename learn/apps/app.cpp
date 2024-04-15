543b0b1b09dc5fa72ff47297e6334109afa2d8a0
dilbud <buddikadilan3@gmail.com>
2024-04-15 15:03:06 +0000

    thrad pool

--- learn/apps/app.cpp
+++ learn/apps/app.cpp
@@ -1,9 +1,37 @@
+extern "C" {
+#include <fcntl.h>
+#include <sys/epoll.h>
+#include <unistd.h>
+}
+
+#include <functional>
 #include <iostream>
+#include <thread>
+#include <vector>
+
+#include "ThreadPool.h"
+
 int main() {
-    std::cout << "Enter two numbers:" << std::endl;
-    int v1 = 0;
-    int v2 = 0;
-    std::cin >> v1 >> v2;
-    std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
+    ThreadPool pool;
+
+    std::vector<std::future<size_t>> vec;
+
+    size_t max = 500000;
+
+    // Enqueue tasks for execution
+    for (size_t i = 0; i < max; ++i) {
+        auto func = [i] {
+            // std::cout << "Task " << i << " is running on thread " << std::this_thread::get_id() << std::endl;
+            // Simulate some work
+            // std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            return i;
+        };
+        vec.push_back(pool.enqueue(func));
+    }
+
+    for (size_t i = 0; i < max; i++) {
+        std::printf("%zd\n", vec.at(i).get());
+    }
+
     return 0;
 }
